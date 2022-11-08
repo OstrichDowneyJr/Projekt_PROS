03414122400a9df4e9dadd1f722e9cc9d9e6bb3e
OstrichDowneyJr <wiewior.niewystraszny@gmail.com>
2022-11-09 00:01:22 +0100

    Plan działana

---
+++ main.c
@@ -0,0 +1,6 @@
+// TODO Napisz co masz wgl do zrobienie w tym projekcie 
+// popros o rozmiar tablicy NxN
+// wypełnij tablice 
+// sprawdz jakie ma klastry wewnatrz
+// ponumeruj kalstry 
+// zaznacz które klastry są przeciekające 
