5f65a3175806f86df5b5ddbd7d7a23963f4a768f
OstrichDowneyJr <wiewior.niewystraszny@gmail.com>
2023-01-25 18:23:06 +0100

    Rozbudowa maina, dopisanie README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-### Projekt na PROS
-
-Wizualizacja perkolacji macierzowej w jezyku C.
+# Idea projektu
+Kod przeprowadzajacy symulacje perkolacji macierzowej w C i Pythonie.
+## Konwencja adresowania tablicy
+W kodzie dla osobistej wygody przyjąłem konwnecje adresowania tylko 1-ną zmienną "__i__".
+Aby uzyskać standardowe adresowanie w układzie kartezjańskim w macierzy przyjąłem transformatę $(x,y)$--->$(i\%n,i/n)$ gdzie $n$ to długość/szerkość macierzy. W takim układzie odpada mi pare problemów z poprawnym nazewnictwem komórek i warunków klastrów. 
+# Mechanizm projektu
+W projekcie tworzę macierz __NxN__ (rozmiar zadany przez urzytkownika) który następnie wypełniam
+jedynkami dla pełnej komórki (prawdopodobieństwo wypełnienia zadane przez urzytkownika) oraz zerami puste komórki. Nastepnie przechodząc po macierzy za pomocą wyznaczonych wcześniej warunków sprawdzam, czy i jak licznie występuja w macierzy klastry o numerach od 2 do $\frac{n^2}{2}$ (_to najgorszy przypadek kiedy tablica jest wypełniona w sposób szachownicy i każda komórka jes wtedy swoim klastrem_) 
+Potem sprawdzana jest macierz zamian, jednowymiarowa lista trzymajaca indeksy klastrów niepoprawnie zaadresowanych. Przechodzimy jeszcze raz po tablicy zmieniając indeksy istniejące w macierzy zmian i mamy poprawnie nazwane kalstry w naszej macierzy.
+Na końcu badam, czy którykolwiek klaster ma elementy na skrajnych "ścianach tablllicy". Jeżeli jedne lub więcej klastrów spełnia ten warunek, są to klastry perkolujące. 
--- main.c
+++ main.c
@@ -1,55 +1,119 @@
-// popros o rozmiar tablicy NxN -- DONE 
-// wypełnij tablice -- DONE 
-// sprawdz jakie ma klastry wewnatrz
-// ponumeruj kalstry 
-// zaznacz które klastry są przeciekające 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 
-#define TAB_SIZE 64
+#define TAB_SIZE 64 // maksymalna dlugosc jednego wymiaru tablicy 
+
+
+// konwencja adresowania (x,y)--->(i%user_size,i/user_size)
 
 int main(void){
 
+    int cluster = 2;
     int treshhold; //? poziom zapelniania komorek z przypadkowej funkcji 
     int user_size; //? rozmiar tablicy wybrany przez urzytkownika
 
-    // rezerwowanie pamiecie dla tablicy
+
+    //? rezerwowanie pamiecie dla tablicy
     int* tab = (char*) malloc(sizeof(int) * TAB_SIZE*TAB_SIZE); // pamiec dla tablicy 
     if(tab == NULL) { 
         printf("Error! memory not allocated.");
         exit(0);
     }
 
-    // wybor rozmiaru tablicy
+    //? wybor rozmiaru tablicy
     printf("Podaj rozmiar tablicy, maks %d\n", TAB_SIZE);
     scanf("%d",&user_size);
-
     while (user_size>TAB_SIZE){
         printf("Rozmiar tablicy za duży, podaj poprawny rozmiar\n");
         scanf("%d",&user_size);
     }
 
-    //inicjowanie przypadkowosci w kodzie 
+    int* tab_zmian = (char*) malloc(sizeof(int) * TAB_SIZE*TAB_SIZE/2); // pamiec dla tablicy 
+        if(tab_zmian == NULL) { 
+            printf("Error! memory not allocated.");
+            exit(0);
+        }
+
+    //? inicjowanie przypadkowosci w kodzie 
     time_t tt;
     int seed = time(&tt); 
     srand(seed);
 
-    //podaj jakie prawdopodobienstwo zapelnienia komorki 
+    //? podaj jakie prawdopodobienstwo zapelnienia komorki 
     printf("Podaj prawdopodobienstwo\n");
     scanf("&d",&treshhold);
 
-    // wypelnianie tablicy
-    for(int i = 0; i < user_size; i++){
-        for (int j = 0; i < user_size; i++){
+    //? wypelnianie tablicy
+    for(int i = 0; i < user_size*user_size; i++){
             int guess = rand()%100;
-            if(guess>treshhold) tab[i*user_size + j]=1; //? wyrazenie w nawaiasach adresuje tablice jako 1-wymarowa do pracy
+            if(guess>treshhold) tab[i]=1; 
+            else tab[i]=0;
+        
+    }
+
+    //?inicjalizacja klastrów
+    for(int i = 0; i < user_size*user_size; i++){
+            if (tab[i]==1){
+                tab[i]=cluster;
+                continue;
         }
     }
 
+    // zmiana nazwenictwa dla wygody czytania
+    int poprzedni = i-1; 
+    int dolny = (i%user_size)-1;
+    int aktualny = i; 
 
+    //? numerowanie klastrow
+    for(int i = 0; i < user_size*user_size; i++){
+            if(tab[aktualny]==1){ // czy wgl pracujemy na wlasciwej komórce 
+                if(tab[poprzedni]==cluster) && (tab[dolny]==cluster){
+                    tab[aktualny]=cluster; 
+                }
+                else if( tab[poprzedni]!=cluster ) && (tab[dolny]==cluster){
+                    tab[aktualny]=cluster;
+                    tab_zmian[poprzedni]=cluster;
+                }
+                else if (tab[dolny]==0) && (tab[poprzedni]==cluster){
+                    tab[aktualny]=cluster;
+                }
+                else if(tab[dolny]==cluster) && (tab[poprzedni]==0){
+                    tab[aktualny]=cluster;
+                }
+                else if(i%user_size==0) && (tab[dolny]==cluster){//przypadek kiedy jestesmy w pierwszej kolumnie
+                    tab[aktualny]=cluster;
+                }
+                else if(i/n==0) && (tab[poprzedni]==cluster){//przypadke kiedy jestesmy w pierwszym rzedzie
+                    tab[aktualny]=cluster;
+                }
+                else{
+                    cluster=+1;
+                    tab[aktualny]=cluster;
+                }
+            }
+        }
+
+
+    //podmiana klastrow niewlasciwie ponumerowanych 
+    for(int k=cluster,k>1,k--){
+        if(tab_zmian[k]!=0){
+            for(int i = 0; i < user_size*user_size; i++){
+                if (tab[i]==k){
+                    tab[i]=tab[k];
+                }   
+            }
+        }
+    }
+    //prerkolacja 
+    //*(i%user_size==0 && i%user_size==user_size-1)
+    //*(i/user_size==0 && i/user_size==user_size-1)
+    for(int k=cluster,k>1,k--){
+        for(int i=0,i<user_size,i++){
+            
+        }
+    }
 
     free(tab);
     return 0; 
